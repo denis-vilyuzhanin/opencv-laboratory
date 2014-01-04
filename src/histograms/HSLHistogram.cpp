f37a1f33113b45bba79f839884129a132b969693
Denys Viliuzhanin <dvilyuzhanin@gmail.com>
2014-01-04 15:03:57 +0200

    Change Colors of HSL Histogram and add sample images

--- src/histograms/HSLHistogram.cpp
+++ src/histograms/HSLHistogram.cpp
@@ -38,14 +38,14 @@ void HSLHistogram::update(Mat image) {
 	bool uniform = true;
 	bool accumulate = false;
 
-	Mat b_hist, g_hist, r_hist;
+	Mat hue_hist, light_hist, saturation_hist;
 
 	/// Compute the histograms:
-	calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange,
+	calcHist(&bgr_planes[0], 1, 0, Mat(), hue_hist, 1, &histSize, &histRange,
 			uniform, accumulate);
-	calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange,
+	calcHist(&bgr_planes[1], 1, 0, Mat(), light_hist, 1, &histSize, &histRange,
 			uniform, accumulate);
-	calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange,
+	calcHist(&bgr_planes[2], 1, 0, Mat(), saturation_hist, 1, &histSize, &histRange,
 			uniform, accumulate);
 
 	// Draw the histograms for B, G and R
@@ -56,27 +56,27 @@ void HSLHistogram::update(Mat image) {
 	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
 
 	/// Normalize the result to [ 0, histImage.rows ]
-	normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+	normalize(hue_hist, hue_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+	normalize(light_hist, light_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+	normalize(saturation_hist, saturation_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
 
 	/// Draw for each channel
 	for (int i = 1; i < histSize; i++) {
 		line(histImage,
 				Point(bin_w * (i - 1),
-						hist_h - cvRound(b_hist.at<float>(i - 1))),
-				Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
+						hist_h - cvRound(hue_hist.at<float>(i - 1))),
+				Point(bin_w * (i), hist_h - cvRound(hue_hist.at<float>(i))),
 				Scalar(255, 0, 0), 2, 8, 0);
 		line(histImage,
 				Point(bin_w * (i - 1),
-						hist_h - cvRound(g_hist.at<float>(i - 1))),
-				Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
-				Scalar(0, 255, 0), 2, 8, 0);
+						hist_h - cvRound(light_hist.at<float>(i - 1))),
+				Point(bin_w * (i), hist_h - cvRound(light_hist.at<float>(i))),
+				Scalar(255, 255, 255), 2, 8, 0);
 		line(histImage,
 				Point(bin_w * (i - 1),
-						hist_h - cvRound(r_hist.at<float>(i - 1))),
-				Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
-				Scalar(0, 0, 255), 2, 8, 0);
+						hist_h - cvRound(saturation_hist.at<float>(i - 1))),
+				Point(bin_w * (i), hist_h - cvRound(saturation_hist.at<float>(i))),
+				Scalar(127, 127, 127), 2, 8, 0);
 	}
 
 	/// Display
