5d95423e2e173790a8ecbb7d6f03e81cad165d95
Denys Viliuzhanin <denys_viliuzhanin@epam.com>
2014-01-14 17:01:52 +0200

    Converting dimensions to color

---
+++ src/dim/FastDimCounter.cpp
@@ -0,0 +1,73 @@
+/*
+ * FastDimCounter.cpp
+ *
+ *  Created on: Jan 14, 2014
+ *      Author: Denys_Viliuzhanin
+ */
+
+#include "FastDimCounter.h"
+#include <math.h>
+
+FastDimCounter::FastDimCounter(int size): size(size), counter(new SimpleCounter()) {
+
+
+}
+
+FastDimCounter::~FastDimCounter() {
+	delete counter;
+}
+
+static cv::Vec3b getValue(const cv::Mat& image, int x, int y) {
+	if (image.channels() == 1) {
+		uchar v = image.at<uchar>(y, x);
+		return cv::Vec3b(v, v, v);
+	} else if (image.channels() == 3) {
+		return image.at<cv::Vec3b>(y, x);
+	}
+	return cv::Vec3b(0, 0, 0);
+}
+
+static double dim(int n, int size) {
+	return log(double(n)) / log(double(size));
+}
+
+
+
+cv::Vec3d FastDimCounter::compute(const cv::Mat& image) {
+	assertImage(image);
+	for (int x = 0; x < size; x++) {
+		for (int y = 0; y < size; y++) {
+			const cv::Vec3b& value = getValue(image, x, y);
+			counter->increment(value);
+		}
+	}
+	const cv::Vec3i& sum = counter->getResult();
+	return cv::Vec3d(dim(sum[0], size), dim(sum[1], size), dim(sum[2], size));
+}
+
+
+void FastDimCounter::assertImage(const cv::Mat& image) {
+}
+
+SimpleCounter::SimpleCounter(): result(0), step(1, 1, 1), zeroColor(0, 0, 0){
+
+}
+
+SimpleCounter::SimpleCounter(const cv::Vec3b& step): result(0, 0, 0), step(step), zeroColor(0, 0, 0) {
+
+}
+
+SimpleCounter::SimpleCounter(const cv::Vec3b& step, const cv::Vec3b& zeroColor):
+		result(0), step(step), zeroColor(zeroColor) {
+
+}
+
+void SimpleCounter::increment(const cv::Vec3b& value) {
+	if (zeroColor != value) {
+		result += step;
+	}
+}
+
+const cv::Vec3i& SimpleCounter::getResult() {
+	return result;
+}
--- src/histograms/FastDIMHistogram.cpp
+++ src/histograms/FastDIMHistogram.cpp
@@ -21,6 +21,10 @@ FastDIMHistogram::~FastDIMHistogram() {
 
 }
 
+static uchar toColor(double v) {
+	return 255 / 2 * v;
+}
+
 void FastDIMHistogram::update(cv::Mat image) {
 	const int SIZE = std::min(image.rows, image.cols);
 
@@ -28,7 +32,7 @@ void FastDIMHistogram::update(cv::Mat image) {
 	const cv::Vec3d& dim = counter.compute(image);
 
 
-	cv::Mat histImage(SIZE , SIZE, CV_8UC3, cv::Scalar(127*dim[2], 127*dim[1], 127*dim[0]));
+	cv::Mat histImage(SIZE , SIZE, CV_8UC3, cv::Scalar(toColor(dim[2]), toColor(dim[1]), toColor(dim[0])));
 	cv::namedWindow("FastDIM Histogram", CV_WINDOW_AUTOSIZE);
 	cv::imshow("FastDIM Histogram", histImage);
 
