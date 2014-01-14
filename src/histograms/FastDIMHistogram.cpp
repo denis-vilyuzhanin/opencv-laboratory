97104b0972a92ac9c5be9977977244b5a9c8c2e1
Denys Viliuzhanin <denys_viliuzhanin@epam.com>
2014-01-14 17:55:00 +0200

    FastDIM with simple dimension calculation

--- src/histograms/FastDIMHistogram.cpp
+++ src/histograms/FastDIMHistogram.cpp
@@ -12,9 +12,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <algorithm>
 
-FastDIMHistogram::FastDIMHistogram(): size(64) {
+FastDIMHistogram::FastDIMHistogram(): size(8) {
 	cv::namedWindow("FastDIM Histogram", CV_WINDOW_AUTOSIZE);
-	cv::createTrackbar("Size", "FastDIM Histogram", &size, 256);
+	cv::createTrackbar("Size", "FastDIM Histogram", &size, 256 - 2);
 }
 
 FastDIMHistogram::~FastDIMHistogram() {
@@ -27,17 +27,19 @@ static uchar toColor(double v) {
 
 void FastDIMHistogram::update(cv::Mat image) {
 	const int TOTAL_SIZE = std::min(image.rows, image.cols);
-	const int SIZE = std::min(size, TOTAL_SIZE);
+	const int SIZE = std::min(size + 2, TOTAL_SIZE);
 
 	cv::Mat histImage(image.rows , image.cols, CV_8UC3, cv::Scalar(0, 0, 0));
 	FastDimCounter counter(SIZE);
 	for(int i = 0; i < TOTAL_SIZE; i += SIZE) {
 		for(int j = 0; j < TOTAL_SIZE; j += SIZE) {
-			const cv::Vec3d& dim = counter.compute(image);
+			Mat subImage = image.colRange(i, std::min(i + SIZE - 1, image.cols))
+								.rowRange(j, std::min(j + SIZE - 1, image.rows));
+			const cv::Vec3d& dim = counter.compute(subImage);
 			cv::Scalar color(toColor(dim[2]), toColor(dim[1]), toColor(dim[0]));
 			cv::rectangle( histImage,
 				           Point( i, j ),
-				           Point( i + size, size),
+				           Point( i + SIZE, j + SIZE),
 				           color,
 				           -1,
 				           8 );
